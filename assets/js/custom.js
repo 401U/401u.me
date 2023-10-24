2da02c655f2ad3a23656b8fff4d9f241db12caee
401U <hi@4o1.to>
2023-10-24 22:41:45 +0800

    chore: remove sakana

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -227,11 +227,4 @@ function checkcdn(){
       )
   );
 }
-Sakana.init({
-  el:         '.sakana-box',     // 启动元素 node 或 选择器
-  scale:      .5,                // 缩放倍数
-  canSwitchCharacter: true,      // 允许换角色
-});
-Sakana.setMute(false);
-
 checkcdn();
